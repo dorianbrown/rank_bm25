2d8a11a20f48f8a5ee6195ad99b31715a3ca6a42
Dorian Brown <be54nc@apm3lc02xv9npjhd2.nl.europe.intranet>
2019-01-22 17:03:25 +0100

    Added bm25_atire algorithm

--- bm25.py
+++ bm25.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import math
+from multiprocessing import cpu_count, Pool
+import numpy as np
+
 
 # 0. Determine what corpus input data should look like. List of lists (containing tokens)
 
@@ -9,8 +12,11 @@ import math
 #   b. average document length
 #   c. each document's length
 
+# import pandas as pd
+# pd.read_pickle("/data/reuters/normalized_text_df.pkl")
+
 class BM25:
-    def __init__(self, corpus):
+    def __init__(self, corpus, algorithm="atire"):
         """
         Parameters
         ----------
@@ -23,9 +29,13 @@ class BM25:
         self.idf = {}
         self.doc_len = []
 
+        self.k1 = 1.5
+        self.b = 0.75
+        self.epsilon = 0.25
+
         self._initialize(corpus)
 
-    def _initialize(self, corpus):
+    def _initialize(self, corpus, algorithm):
         """Calculates frequencies of terms in documents and in corpus. Also computes inverse document frequencies."""
         nd = {}  # word -> number of documents with word
         num_doc = 0
@@ -40,27 +50,73 @@ class BM25:
                 frequencies[word] += 1
             self.doc_freqs.append(frequencies)
 
-            for word, freq in iteritems(frequencies):
+            for word, freq in frequencies.items():
                 if word not in nd:
                     nd[word] = 0
                 nd[word] += 1
 
-        self.avgdl = float(num_doc) / self.corpus_size
+        self.avgdl = num_doc / self.corpus_size
         # collect idf sum to calculate an average idf for epsilon value
         idf_sum = 0
         # collect words with negative idf to set them a special epsilon value.
         # idf can be negative if word is contained in more than half of documents
         negative_idfs = []
-        for word, freq in enumerate(nd):
+
+        if algorithm == "atire":
+            self.idf = self.calc_atire_idf
+
+        for word, freq in nd.items():
             idf = math.log(self.corpus_size - freq + 0.5) - math.log(freq + 0.5)
             self.idf[word] = idf
             idf_sum += idf
             if idf < 0:
                 negative_idfs.append(word)
-        self.average_idf = float(idf_sum) / len(self.idf)
+        self.average_idf = idf_sum / len(self.idf)
 
-        eps = EPSILON * self.average_idf
+        eps = self.epsilon * self.average_idf
         for word in negative_idfs:
+            self.idf[word] = eps
 
+    def get_atire_scores(self, query):
+        """
+        The ATIRE BM25 variant uses an idf function which uses a log(idf) score. To prevent negative idf scores,
+        this algorithm also adds a floor to the idf value of epsilon.
+        See [Trotman, A., X. Jia, M. Crane, Towards an Efficient and Effective Search Engine] for more info
+        :param query:
+        :return:
+        """
+        score = np.zeros(self.corpus_size)
+        doc_len = np.array(self.doc_len)
+        for q in query:
+            print(f"Query term=: {q}")
+            q_freq = np.array([(doc.get(q) or 0) for doc in self.doc_freqs])
+            print(f"IDF of q: {self.idf[q] or 0}")
+            print(f"Document frequency: {q_freq}")
+            print(f"Document length: {doc_len}")
+            print(f"Avg Doc Length: {self.avgdl}")
+            score += (self.idf[q] or 0) * (q_freq * (self.k1 + 1) /
+                                           (q_freq + self.k1 * (1 - self.b + self.b * doc_len / self.avgdl)))
 
-        self.idf[word] = eps
+        return score
+
+    def get_bm25l_scores(self, query):
+        return
+
+    def get_bm25plus_scores(self, query):
+        return
+
+    def get_bm25adpt_scores(self, query):
+        return
+
+    def get_bm25t_scores(self, query):
+        return
+
+    def get_bm25rousvar_scores(self, query):
+        """
+        Rousseau, F., M. Vazirgiannis, Composition of TF normalizations: new insights on scoring functions for ad hoc IR
+        :param query:
+        :return:
+        """
+        return
+
+class BM25_atire(BM25):
--- tests/test_loading.py
+++ tests/test_loading.py
@@ -3,7 +3,15 @@ from bm25 import BM25
 corpus1 = ["A A B B C", "B B C D"]
 corpus1 = [s.split(" ") for s in corpus1]
 
+corpus2 = ["The quick brown fox", "The slow brown dog", "Jumping jack flash"]
+corpus2 = [s.split(" ") for s in corpus2]
+
+
 def test_corpus_loading():
     bm25_1 = BM25(corpus1)
 
-    assert(BM25)
+    assert (bm25_1.corpus_size == 2)
+    assert (bm25_1.avgdl == 4.5)
+    assert (bm25_1.doc_freqs == [{'A': 2, 'B': 2, 'C': 1}, {'B': 2, 'C': 1, 'D': 1}])
+    assert (bm25_1.idf == {'A': 0.0, 'B': -0.20117973905426256, 'C': -0.20117973905426256, 'D': 0.0})
+    assert (bm25_1.doc_len == [5, 4])
