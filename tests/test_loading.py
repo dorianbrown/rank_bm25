ad3b13a9e1b1c9a7486ed3c55f270480f4d75c79
Dorian Brown <dorianstuartbrown@gmail.com>
2019-04-10 17:39:55 +0200

    Updated travis for pypi deploy

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -5,10 +5,8 @@ sys.path.insert(0, myPath + '/../')
 
 from rank_bm25 import BM25Okapi, BM25L, BM25Plus
 import re
-from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
-from time import time
-from multiprocessing import cpu_count
+
 
 corpus = [
     "Hello there good man!",
@@ -17,24 +15,21 @@ corpus = [
 ]
 tokenized_corpus = [doc.split(" ") for doc in corpus]
 
+algs = [
+    BM25Okapi(tokenized_corpus),
+    BM25L(tokenized_corpus),
+    BM25Plus(tokenized_corpus)
+]
+
 
 def tokenizer(doc):
     doc = doc.lower()
     doc = re.sub(r"[^\w\s]+", "", doc)
     words = word_tokenize(doc)
-
-    ps = PorterStemmer()
-    return [ps.stem(w) for w in words]
+    return words
 
 
 def test_corpus_loading():
-
-    algs = [
-        BM25Okapi(tokenized_corpus),
-        BM25L(tokenized_corpus),
-        BM25Plus(tokenized_corpus)
-    ]
-
     for alg in algs:
         assert alg.corpus_size == 3
         assert alg.avgdl == 5
@@ -46,12 +41,3 @@ def test_tokenizer():
     assert bm25.corpus_size == 3
     assert bm25.avgdl == 5
     assert bm25.doc_len == [4, 6, 5]
-
-
-def test_tokenizer_speed():
-    # Parsing 100k documents shouldn't take more than 5s
-    t0 = time()
-    BM25Okapi(corpus[:1]*100000, tokenizer=tokenizer)
-    t1 = time()
-    exec_time = t1-t0
-    assert exec_time < 50/cpu_count()
