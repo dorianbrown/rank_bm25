a7330b81b9974827b3ba0e0e9b4b6a3af3e70027
Sarthak Jain <54593458+Sarthakjain1206@users.noreply.github.com>
2020-05-28 03:52:57 +0530

    Updated _initialize(self,corpus) function
    Hey, I have improvised the initialize function. The previous implementation was taking a lot of time due to unnecessary searching for 'word' if present in 'nd'. In time complexity terms, it was taking O(n) time for every outer iteration. Whereas we can do that part in O(1) time.
    
    Test it out by taking a huge corpus, In that case, you will see a lot of time difference.
    And, this time difference matters a lot for searching.

--- rank_bm25.py
+++ rank_bm25.py
@@ -42,9 +42,16 @@ class BM25:
             self.doc_freqs.append(frequencies)
 
             for word, freq in frequencies.items():
-                if word not in nd:
+                # It reduces time complexity of this function.
+                try:
+                    nd[word]+=1
+                except:
                     nd[word] = 0
-                nd[word] += 1
+                    nd[word] += 1
+
+#                 if word not in nd:
+#                     nd[word] = 0
+#                 nd[word] += 1
 
         self.avgdl = num_doc / self.corpus_size
         return nd
